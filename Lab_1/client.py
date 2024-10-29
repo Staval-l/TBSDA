4d3c2a2fca484852117cdd29eff7f4ee2d6d86a3
Staval <stalkervaultboy@gmail.com>
2024-10-29 14:12:36 +0400

    Доделал 1 лабу

--- Lab_1/client.py
+++ Lab_1/client.py
@@ -2,26 +2,28 @@ import cv2
 import numpy as np
 import requests
 
+
 def add_gaussian_noise(image):
     mean = 0
-    disp = np.var(image)
     sigma = 0.36
-    gauss = np.random.normal(mean, np.sqrt(disp/10), image.shape).astype('uint8')
+    gauss = np.random.normal(mean, sigma, image.shape).astype('uint8')
     noisy_image = cv2.add(image, gauss)
     return noisy_image
 
-# Загрузка изображения
-image = cv2.imread('images/07_elaine.tif')
 
-# Добавление гауссовского шума
-noisy_image = add_gaussian_noise(image)
+def main():
+    image = cv2.imread('images/07_elaine.tif')
 
-# Сохранение зашумленного изображения
-cv2.imwrite('noisy_image.jpg', noisy_image)
+    noisy_image = add_gaussian_noise(image)
 
-# Отправка изображения на сервер
-url = 'http://127.0.0.1:5000/upload'
-files = {'file': open('noisy_image.jpg', 'rb')}
-response = requests.post(url, files=files)
+    cv2.imwrite('noisy_image.jpg', noisy_image)
 
-print(response.text)
+    url = 'http://127.0.0.1:5000/upload'
+    files = {'file': open('noisy_image.jpg', 'rb')}
+    response = requests.post(url, files=files)
+
+    print(response.text)
+
+
+if __name__ == '__main__':
+    main()
--- Lab_1/server.py
+++ Lab_1/server.py
@@ -4,7 +4,9 @@ import numpy as np
 import cv2
 import io
 
+
 app = Flask(__name__)
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -15,18 +17,16 @@ def upload_file():
     if file.filename == '':
         return 'No selected file', 400
 
-    # Чтение изображения из файла
     file_stream = io.BytesIO(file.read())
     noisy_image = cv2.imdecode(np.frombuffer(file_stream.read(), np.uint8), cv2.IMREAD_COLOR)
 
-    # Удаление шума с помощью медианного фильтра
-    denoised_image = median_filter(noisy_image, size=5)  # 5 - размер ядра
+    restored_image = median_filter(noisy_image, size=5)
 
-    # Сохранение очищенного изображения на сервере
-    output_path = 'denoised_image.jpg'
-    cv2.imwrite(output_path, denoised_image)
+    output_path = 'restored_image.jpg'
+    cv2.imwrite(output_path, restored_image)
 
-    return f'Image denoised and saved as {output_path}', 200
+    return f'Image restored and saved as {output_path}', 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
